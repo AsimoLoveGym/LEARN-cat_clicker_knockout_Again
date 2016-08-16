3b4158f4f996caa4e564b5ff69effe4130b2b30c
AsimoLoveGym <xiazhuangz@gmail.com>
2016-08-16 11:09:04 -0500

    Finished the whole practice, pretty fun

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+var initialCats = [
+{
+  clickCount: 0,
+  name: "Ross",
+  imgSrc: "img/Ross.jpg",
+  imgAttribution: "",
+  nicknames: ['Roro','SS','Little Dinosor']
+},
+{
+  clickCount: 0,
+  name: "Chandler",
+  imgSrc: "img/Chandler.jpg",
+  imgAttribution: "",
+  nicknames: ['Bing','OMG','Fun Guy']
+},
+{
+  clickCount: 0,
+  name: "Joey",
+  imgSrc: "img/Joey.jpg",
+  imgAttribution: "",
+  nicknames: ['Little General','Playboy','Italian boy']
+},
+{
+  clickCount: 0,
+  name: "Phoebe",
+  imgSrc: "img/Phoebe.jpg",
+  imgAttribution: "",
+  nicknames: ['FUNFUN','Phoephoe','Smelly Cat']
+},
+{
+  clickCount: 0,
+  name: "Monica",
+  imgSrc: "img/Monica.jpg",
+  imgAttribution: "",
+  nicknames: ['Momo','Chief','Lala']
+},
+{
+  clickCount: 0,
+  name: "Rachel",
+  imgSrc: "img/Rachel.jpg",
+  imgAttribution: "",
+  nicknames: ['Rachel','Shoe','Pretty Girl']
+}
+]
+
 var Cat = function(data){
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -26,15 +71,22 @@ var Cat = function(data){
 
 var ViewModel = function(){
   var self = this;
-  this.currentCat = ko.observable(new Cat({
-    clickCount: 0,
-    name: 'Joey',
-    imgSrc: 'img/Joey.jpg',
-    nicknames:['Chouchou','ErHa','Asimo']
-  }));
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function(){
     self.currentCat().clickCount(self.currentCat().clickCount()+1);
+  };
+
+  this.setCat = function(){
+    self.currentCat(this);
+    // console.log('hi');
   };
 }
 
